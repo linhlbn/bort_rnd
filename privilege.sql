2d9cc6b3ccada65f0833c3306e9e859793a429eb
jlp <49004582+linhlbn@users.noreply.github.com>
2024-01-03 23:15:45 +0700

    Update privilege.sql

--- privilege.sql
+++ privilege.sql
@@ -6,22 +6,37 @@ CREATE TABLE if not exists DocumentRoleAccess (
     FOREIGN KEY (role_id) REFERENCES Role(role_id)
 );
 
--- 'company_info'
+-- 'company_info' 
 INSERT INTO DocumentRoleAccess (doc_type_id, role_id) VALUES
-(1, 1), --  Manager
-(1, 2), --  Senior
-(1, 3), --  Employee
-(1, 4); --  New Employee
+(1, 1),  -- Chief Executive Officer (CEO)
+(1, 2),  -- Chief Technology Officer (CTO)
+(1, 3),  -- Chief Product Officer (CPO)
+(1, 4),  -- Chief Growth Officer (CGO)
+(1, 5),  -- UI/UX Specialist
+(1, 6),  -- Operations Specialist
+(1, 7),  -- Senior Mobile Developer
+(1, 8),  -- BD associate
+(1, 9),  -- Junior Web Developer
+(1, 10), -- Junior Web / Wordpress Developer
+(1, 11), -- UI/UX Intern
+(1, 12); -- Developer Intern
 
--- 'company_project'
+-- 'company_project' - 1, 2, 3, 4
 INSERT INTO DocumentRoleAccess (doc_type_id, role_id) VALUES
-(2, 1); --  Manager
+(2, 1),  -- Chief Executive Officer (CEO)
+(2, 2),  -- Chief Technology Officer (CTO)
+(2, 3),  -- Chief Product Officer (CPO)
+(2, 4);  -- Chief Growth Officer (CGO)
 
--- 'candidate_cv'
+-- 'candidate_cv' - 1, 2, 3, 4
 INSERT INTO DocumentRoleAccess (doc_type_id, role_id) VALUES
-(3, 1), -- Manager
-(3, 2); -- Senior
+(3, 1),  -- Chief Executive Officer (CEO)
+(3, 2),  -- Chief Technology Officer (CTO)
+(3, 3),  -- Chief Product Officer (CPO)
+(3, 4);  -- Chief Growth Officer (CGO)
 
-select * from privilege;
+select * from Role;
 select * from Documenttype;
 select * from DocumentRoleAccess;
+
+-- document in detail about flow related to chatbot will be updated later
