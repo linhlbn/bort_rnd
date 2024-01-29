a367e26cea053f3d74a9e6da7e69e2ca98d6fab5
jlp <49004582+linhlbn@users.noreply.github.com>
2024-01-29 16:01:21 +0700

    Create test_description.py

---
+++ test_description.py
@@ -0,0 +1,45 @@
+
+"""
+
+Code: 1 CI, 2 CP, 3 CR, 4 CO
+    CI: ompany Information
+    CP: Company Project
+    CR: Company Resume (CV)
+    CO: Company Onboarding
+
+
+Cloud Storage structured:
+
+    Company Information
+        ...
+    
+    Company Project
+        ...
+
+    Company Policies
+        ...
+
+"""
+
+
+## chỉ cần làm CI, CP
+
+# doc_[code]_[name document]
+doc_CI_template = '''
+    <description for template document>
+
+'''
+doc_CI_template_2 = '''
+    <description for template document 2>
+
+'''
+
+
+
+
+list_doc_CI = [doc_CI_template, doc_CI_template_2]
+
+
+
+# test
+print('hello world')
