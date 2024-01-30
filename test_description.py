7d51fb0cc33d85188e7df901eadd626c067ca0bb
Le Quang Hai Dang <88619108+lquhaidang@users.noreply.github.com>
2024-01-30 12:27:41 +0700

    Update test_description.py

--- test_description.py
+++ test_description.py
@@ -2,7 +2,7 @@
 """
 
 Code: 1 CI, 2 CP, 3 CR, 4 CO
-    CI: ompany Information
+    CI: Company Information
     CP: Company Project
     CR: Company Resume (CV)
     CO: Company Onboarding
@@ -19,25 +19,72 @@ Cloud Storage structured:
     Company Policies
         ...
 
-"""
+""" 
 
 
 ## chỉ cần làm CI, CP
 
 # doc_[code]_[name document]
 doc_CI_template = '''
-    <description for template document>
-
-'''
-doc_CI_template_2 = '''
-    <description for template document 2>
-
+    Orthian is your trusted digital partner for business success through innovation. We specialize in Business Innovation + AI, Website Design & Development, Software Development, IT Services, and Marketing Design. 
+    Our client-centric approach ensures customized solutions for businesses of all sizes. With Orthian, you gain more than services; you gain collaborative expertise and future-focused strategies. 
+    From transforming your digital landscape to holistic Business Transformation, we're here to drive your growth and success.
 '''
 
+doc_CP_template_1 = '''
+    Orthian collaborates with Bạch Kiếm, a premier furniture photography service. 
+    In Project Code BK-001, we enhance Bạch Kiếm's brand with a logo redesign, website reconstruction, and basic SEO. 
+    Our goal is to elevate Bạch Kiếm's online presence, improve UI/UX, and ensure easy user discovery. 
+    Together, we're shaping a distinct identity for Bạch Kiếm in the furniture industry.
+'''
 
+doc_CP_template_2 = '''
+    Orthian is collaborating on Project EcoSwap (ES-001) led by Sơn Nguyễn and a team including Vương Nguyễn, Hải Lê, Hải Tô, Vy Võ, and Thọ Phạm. 
+    This long-term initiative aims to revolutionize the mobile device trade-in process through ES-C.App, ES-PWA, ES-Admin, and ES-Website. 
+    Customers can easily self-check device condition and value using the mobile app, streamlining trade-ins with store assistance. 
+    Orthian is committed to making EcoSwap a seamless and innovative solution in the smart device trade-in service industry.
+'''
 
+doc_CP_template_3 = '''
+    Orthian collaborates on Project ECS-001, led by Hai To and Tho Pham, for Energy Construction And Services Co., Ltd. (ECS) specializing in wind and solar energy. 
+    This short-term project centers on designing a website to provide stakeholders with detailed insights into ECS's operations, services, and projects, strengthening its presence in the renewable energy sector.
+'''
 
-list_doc_CI = [doc_CI_template, doc_CI_template_2]
+doc_CP_template_4 = '''
+    Orthian collaborates on Project GAB-001, led by Hải Tô, focusing on the website design for Global A&B Investment and Development Company Limited. 
+    Specializing in the supply and processing of various steel products, the short-term project aims to provide stakeholders with a comprehensive view of GAB's activities, services, and projects through an informative website.
+'''
+
+doc_CP_template_5 = '''
+    Orthian is collaborating on Project GAB-001, led by Hải Tô, alongside Thọ Phạm and Vy Võ. 
+    The short-term project focuses on website design for Global A&B Investment and Development Company Limited, a leader in supplying and processing various steel products, including cold-rolled steel, hot-rolled galvanized steel, and stainless steel. 
+    The objective is to create a comprehensive website to inform stakeholders, such as businesses and organizations, about GAB's activities, services, and projects, fostering a deeper understanding of the company.
+'''
+
+doc_CP_template_6 = '''
+    Orthian collaborates on Project JD-006 led by Hải Tô and Thọ Phạm for the MJ Hararei Website. Focused on luxury property rental services in New Zealand, the short-term project involves website design and API setup. 
+    The goal is to create a user-friendly platform showcasing MJ Hararei's commercial real estate, providing detailed property information and facilitating effective communication with potential customers for a smooth rental process.
+'''
+
+doc_CP_template_7 = '''
+    Orthian is actively engaged in Project LL-001, the SpecTrum Website, led by Hải Tô and Thọ Phạm. 
+    This short-term initiative focuses on creating a comprehensive online platform for Leong Lee International Limited, a distinguished player in the high-end eyewear business. 
+    SpecTrum ("SP") is a premium eyewear brand offering a diverse range of products, from prescription solutions to unique fashion choices. 
+    The goal is to provide detailed and accurate information about SpecTrum to customers, partners, and potential investors, fostering a deep understanding of the brand, its operations, and outstanding product offerings. 
+    Orthian is committed to enhancing SpecTrum's online presence and creating an informative and engaging platform for stakeholders.
+'''
+
+doc_CP_template_8 = '''
+    Project Code: Orthian Website  
+    Project Owner: Hải Lê  
+    Collaborators: 
+    - Tráng Nguyễn  
+    - Quân Nguyễn  
+
+The Orthian Website project is led by owner Hải Lê, with collaboration from team members Tráng Nguyễn and Quân Nguyễn.
+'''
+
+list_doc_CI = [doc_CI_template, doc_CP_template_1, doc_CP_template_2, doc_CP_template_3, doc_CP_template_4, doc_CP_template_5, doc_CP_template_6, doc_CP_template_7, doc_CP_template_8]
 
 
 
